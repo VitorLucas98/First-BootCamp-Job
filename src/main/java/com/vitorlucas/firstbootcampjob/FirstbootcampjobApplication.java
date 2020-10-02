df117e85ff5e7d3af6c864c946d24e037abca918
Vitor Lucas <vitorluc.2013@gmail.com>
2020-10-02 16:05:16 -0300

    Project created

---
+++ src/main/java/com/vitorlucas/firstbootcampjob/FirstbootcampjobApplication.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.firstbootcampjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstbootcampjobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstbootcampjobApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorlucas/firstbootcampjob/FirstbootcampjobApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlucas.firstbootcampjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstbootcampjobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
